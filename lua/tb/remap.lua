af756731876b52d82cef20e111f6a4b5b3c80621
abu-hiba <tbolton96@icloud.com>
2024-01-03 15:33:00 +0000

    add remap to show buffer list and select buffer

--- lua/tb/remap.lua
+++ lua/tb/remap.lua
@@ -5,6 +5,9 @@ vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>t", vim.cmd.Tex)
 vim.keymap.set("n", "<leader>v", vim.cmd.Vex)
 
+-- List and select from buffers
+vim.keymap.set("n", "<leader>b", ":ls<CR>:b<Space>")
+
 -- Terminal
 vim.keymap.set("n", "<leader>z", ":belowright split | resize 20 | term <CR> :startinsert <CR>")
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
