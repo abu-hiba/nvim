3e97265fc845a823fe9fe2d01ebc99b075e5cd9f
abu-hiba <tbolton96@icloud.com>
2024-01-19 18:21:08 +0000

    Remove white space

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -2,7 +2,7 @@ require("lualine").setup {
     options = {
         component_separators = "|",
         -- component_separators = { left = "\\", right = "/" },
-        section_separators = { left = "", right = "" },  
+        section_separators = { left = "", right = "" },
         -- section_separators = { left = "", right = "" },  
     },
     sections = {
