822fdb58035c575b5fe8912eea5ce0020a65797f
abu-hiba <tbolton96@icloud.com>
2024-02-06 15:26:13 +0000

    Add lualine tabline config

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -6,7 +6,7 @@ require("lualine").setup {
         -- section_separators = { left = "", right = "" },
     },
     sections = {
-        lualine_a = { { "mode", icon = " " } },
+        lualine_a = { "mode" },
         lualine_b = { { "branch", icon = "" }, "diff", "diagnostics" },
         lualine_c = { "%<%f" },
         lualine_x = { "encoding", "fileformat", "filetype" },
@@ -18,6 +18,20 @@ require("lualine").setup {
         lualine_b = {},
         lualine_c = { "%<%f" },
         lualine_x = { "location" },
+        lualine_y = {},
+        lualine_z = {}
+    },
+    tabline = {
+        lualine_a = { function() return "" end },
+        lualine_b = {
+            {
+                "tabs",
+                mode = 1,
+                use_mode_colors = true,
+            }
+        },
+        lualine_c = {},
+        lualine_x = {},
         lualine_y = {},
         lualine_z = {}
     },
