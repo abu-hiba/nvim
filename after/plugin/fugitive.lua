8b401de2f96ac1621bb261d5f6d67a305394b3f2
abu-hiba <tbolton96@icloud.com>
2024-02-08 17:03:09 +0000

    Adds more git command shortcuts

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -7,16 +7,29 @@ local function branch_name()
     end
 end
 
+local function get_branches()
+  local output = vim.fn.system("git branch")
+  local branches = {}
+  for branch in string.gmatch(output, "([^%s]+)") do
+    if branch ~= "*" then
+      table.insert(branches, branch)
+    end
+  end
+  return branches
+end
+
 local function git_push_set_upstream()
     local branch = branch_name()
     return vim.cmd("Git push -u origin " .. branch)
 end
 
-local function git_switch(args)
+local function git_switch(opts)
+    local args = opts.fargs
     return vim.cmd("Git switch " .. args[1])
 end
 
-local function git_switch_create(args)
+local function git_switch_create(opts)
+    local args = opts.fargs
     return vim.cmd("Git switch -c " .. args[1])
 end
 
@@ -29,6 +42,6 @@ vim.keymap.set("n", "<leader>gp", ":Git push<CR>")
 vim.keymap.set("n", "<leader>gl", ":Git pull<CR>")
 vim.keymap.set("n", "<leader>gb", ":Git branch<CR>")
 vim.api.nvim_create_user_command('Gpsup', git_push_set_upstream, {})
-vim.api.nvim_create_user_command('Gsw', git_switch, {})
-vim.api.nvim_create_user_command('Gswc', git_switch_create, {})
+vim.api.nvim_create_user_command('Gsw', git_switch, { nargs = 1, complete = get_branches })
+vim.api.nvim_create_user_command('Gswc', git_switch_create, { nargs = 1 })
 vim.api.nvim_create_user_command('Gswm', git_switch_main, {})
