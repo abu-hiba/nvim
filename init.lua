06b8fc76a3e37046aac7944a368be42225336b7b
Thomas Bolton <tbolton96@icloud.com>
2026-06-29 13:25:15 +0100

    add telescope

--- init.lua
+++ init.lua
@@ -31,7 +31,21 @@ require("lazy").setup({
   "williamboman/mason.nvim",         -- Optional: Installer for LSPs
   "williamboman/mason-lspconfig.nvim",
   "lewis6991/gitsigns.nvim",
+  {
+      'nvim-telescope/telescope.nvim', version = '*',
+      dependencies = {
+          'nvim-lua/plenary.nvim',
+          { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+      }
+  }
 })
+
+-- Telescope Setup
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
 -- Mason Setup
 require("mason").setup()
